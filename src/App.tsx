a8f8c2808a1709ed91b9b39ef8de7c17523f13a7
yelkanat <52521184+yelkanat@users.noreply.github.com>
2020-07-28 22:22:10 +0600

    Сделал роутинг по компонентам и импортировал BrowserRouter, Switch, Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import { MainPageCards } from "./components/Cards/Cards";
+import About from "./containers/About/About";
+import { Maps } from "./containers/Map/Map";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Layout>
+          <Switch>
+            <Route component={MainPageCards} path="/" exact />
+            <Route component={About} path="/about" exact />
+            <Route component={Maps} path="/map" exact />
+          </Switch>
+        </Layout>
+      </div>
+    </BrowserRouter>
   );
 }
 
